bb32970298e9c99a5eeeab43eec6810b45d55d58
Brian Joseph <brianjosephstudio@gmail.com>
2024-04-01 12:07:15 +0200

    Error handling middleware

--- src/util/middleware/ErrorHandler.ts
+++ src/util/middleware/ErrorHandler.ts
@@ -2,6 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err);
+    if(err.message === "Wrong query params"){
+        res.status(400).json({
+            error: err.message,
+            message: "On or more query params don't match the expected format"
+        })
+        return next()
+    }
     res.status(500).json({ error: "Internal Server Error" });
     next()
 };
